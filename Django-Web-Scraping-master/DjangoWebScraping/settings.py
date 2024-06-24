2cb95a8800b442b36d39a4cb92b8810fadb5d954
Abhishek Rajput <abhishek25022004@gmail.com>
2024-06-25 01:08:35 +0530

    Modified settings.py

--- Django-Web-Scraping-master/DjangoWebScraping/settings.py
+++ Django-Web-Scraping-master/DjangoWebScraping/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wa810zt$=t+ei!_tv7m+kur^l=vyh)a%9^v=jq@!_o64yo5bck'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
