bc7a791137c8fc1c550fcde4878983f621d0b6d5
Abhishek Rajput <abhishek25022004@gmail.com>
2024-06-25 01:25:43 +0530

    a

--- Django-Web-Scraping-master/build_files.sh
+++ Django-Web-Scraping-master/build_files.sh
@@ -1,3 +1,6 @@
 # build_files.sh
+pip install virtualenv
+virtualenv newenv
+source newenv/bin/activate
 python3.9 -m pip install -r requirement.txt
 python3.9 manage.py collectstatic  --noinput --clear
